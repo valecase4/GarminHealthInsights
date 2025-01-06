c59bb278fc6d897736d6f0f970d6048a87203e1e
valecase4 <valeriocaselli50@gmail.com>
2025-01-06 16:19:55 +0100

    Getted the data about heart rate values during sleep. Updated data.md file.

--- script.py
+++ script.py
@@ -75,7 +75,23 @@ def fetch_sleep_stages_data(client, date):
         return sleep_data_stages
     
     except Exception as e:
-        print(f"Error fetching sleep data: {e}")
+        print(f"Error fetching sleep data (stages): {e}")
+        return None
+
+def fetch_sleep_heart_rates_data(client, date):
+    try:
+        data = client.get_sleep_data(date)['sleepHeartRate']
+        heart_rates_sleep_data = []
+        for record in data:
+            heart_rate = record['value']
+            timestamp = datetime.fromtimestamp(int(record['startGMT']) / 1000)
+            timestamp = timestamp.strftime("%H:%M")
+            heart_rates_sleep_data.append([heart_rate, timestamp])
+        
+        return heart_rates_sleep_data
+
+    except Exception as e:
+        print(f"Error fetching sleep data (heart rates): {e}")
         return None
     
 def save_to_csv(data, filename, columns, look_for_duplicates=True):
@@ -151,7 +167,18 @@ def main(start_date="2024-12-21"):
             ["start_time", "end_time", "sleep_stage"],
             look_for_duplicates=True
         )
-    
+
+    # SLEEP HEART RATES
+    for date in date_list:
+        sleep_heart_rates_data = fetch_sleep_heart_rates_data(client, date)
+        save_to_csv(
+            sleep_heart_rates_data,
+            f"data/raw/sleep/heart_rates/{date}.csv",
+            ["timestamp", "heart_rate"],
+            look_for_duplicates=True
+        )
+
+    # STEPS
     steps_data = []
     for date in date_list:
         daily_steps = fetch_steps(client, date)
--- test/get_sleep_data.py
+++ test/get_sleep_data.py
@@ -26,22 +26,22 @@ def fetch_sleep_data_stages(client, date):
         0: "Awake"
     }
     try:
-        data = client.get_sleep_data(date)['sleepLevels']
+        data = client.get_sleep_data(date)['sleepHeartRate']
         sleep_data_stages = []
         for record in data:
             print(record)
-            start_gmt_time = datetime.strptime(record['startGMT'], "%Y-%m-%dT%H:%M:%S.%f")
-            start_local_time = (start_gmt_time + timedelta(hours=1)).strftime("%H:%M")
-            end_gtm_time = datetime.strptime(record['endGMT'], "%Y-%m-%dT%H:%M:%S.%f")
-            end_local_time = (end_gtm_time + timedelta(hours=1)).strftime("%H:%M")
-            sleep_stage = stages[int(record['activityLevel'])]
+            # start_gmt_time = datetime.strptime(record['startGMT'], "%Y-%m-%dT%H:%M:%S.%f")
+            # start_local_time = (start_gmt_time + timedelta(hours=1)).strftime("%H:%M")
+            # end_gtm_time = datetime.strptime(record['endGMT'], "%Y-%m-%dT%H:%M:%S.%f")
+            # end_local_time = (end_gtm_time + timedelta(hours=1)).strftime("%H:%M")
+            # sleep_stage = stages[int(record['activityLevel'])]
 
-            print([start_local_time, end_local_time, sleep_stage])
+            # print([start_local_time, end_local_time, sleep_stage])
 
-            sleep_data_stages.append([start_local_time, end_local_time, sleep_stage])
+            # sleep_data_stages.append([start_local_time, end_local_time, sleep_stage])
 
         print(f"Sleep data retrieved for {date}.")
-        return sleep_data_stages
+        # return sleep_data_stages
     except Exception as e:
         print(f"Error fetching sleep data: {e}")
         return None
@@ -52,8 +52,7 @@ def fetch_sleep_data_stages(client, date):
 if __name__ == '__main__':
     client = authenticate()
 
-    sleep_data_stages = fetch_sleep_data_stages(client, date='2025-01-06')
-    print(sleep_data_stages)
+    fetch_sleep_data_stages(client, date='2025-01-06')
 
 
     # for record in sleep_data:
